086548741229dcb5fb106e5d478ea679950a2d51
shansen5 <steven.e.hansen@gmail.com>
2017-07-03 23:07:13 -0700

    Add use of reducer.

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,10 +4,24 @@ console.log( 'Starting redux example' );
 
 var reducer = ( state = { name: 'Anonymous' }, action ) => {
     // state = state || { name: 'Anonymous' };  ES5 way
-    return state;
+    switch( action.type ) {
+        case 'CHANGE_NAME':
+            return {
+                ...state,
+                name: action.name
+            };
+        default:
+            return state;
+    }
 };
 
 var store = redux.createStore( reducer );
 
 var currentState = store.getState();
 console.log( 'currentState', currentState );
+
+store.dispatch( {
+    type: 'CHANGE_NAME',
+    name: 'Steve'
+} );
+console.log( 'Name changed', store.getState() );
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -3,15 +3,28 @@ var redux = require( 'redux' );
 console.log( 'Starting redux todo example' );
 
 var reducer = ( state = { 
-    searchText: '',
-    showCompleted: false,
-    todos: [] 
+        searchText: '',
+        showCompleted: false,
+        todos: [] 
     }, action ) => {
-    // state = state || { name: 'Anonymous' };  ES5 way
-    return state;
+        switch( action.type ) {
+            case 'CHANGE_SEARCH_TEXT':
+                return {
+                    ...state,
+                    searchText: action.searchText
+                }
+            default:
+                return state;
+        }
 };
 
 var store = redux.createStore( reducer );
 
 var currentState = store.getState();
 console.log( 'currentState', currentState );
+
+store.dispatch( {
+    type: 'CHANGE_SEARCH_TEXT',
+    searchText: 'boo'
+} );
+console.log( 'Search text changed', store.getState() );
